4bb8d3a17c3577ad9b9a7f4c30ffec37c894c7ef
Adam Lewis <mr_adam_lewis@sky.com>
2016-01-11 13:04:35 +0000

    Initial Commit

---
+++ Route Buddy/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Route Buddy
+//
+//  Created by Adam Lewis on 11/01/2016.
+//  Copyright © 2016 Harry and Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Route Buddy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Route Buddy
+//
+//  Created by Adam Lewis on 11/01/2016.
+//  Copyright © 2016 Harry and Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
