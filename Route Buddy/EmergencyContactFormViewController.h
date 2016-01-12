eb1151d07332269cba460e47826a145bf63a87ae
Adam Lewis <mr_adam_lewis@sky.com>
2016-01-12 14:51:16 +0000

    Keyboard now opens and closes when return is clicked on emergency contact form

--- Route Buddy/EmergencyContactFormViewController.h
+++ Route Buddy/EmergencyContactFormViewController.h
@@ -6,10 +6,22 @@
 //  Copyright © 2016 Harry and Adam. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#import "EmergencyContact.h"
 
-@interface EmergencyContactFormViewController : UIViewController
+@interface EmergencyContactFormViewController : UIViewController <UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
 
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTextField;
+
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
+
+@property (weak, nonatomic) IBOutlet UINavigationItem *navigationTitle;
+
+@property (strong) EmergencyContact *emergencyContact;
+
+- (void) onSubmitButtonClick;
 
 @end
---
+++ Route Buddy/EmergencyContactFormViewController.m
@@ -0,0 +1,48 @@
+//
+//  EmergencyContactFormViewController.m
+//  Route Buddy
+//
+//  Created by Adam Lewis on 12/01/2016.
+//  Copyright © 2016 Harry and Adam. All rights reserved.
+//
+
+#import "EmergencyContactFormViewController.h"
+
+@implementation EmergencyContactFormViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Set the delegates for the text field to this so return hides the keyboard
+    self.nameTextField.delegate = self;
+    self.phoneNumberTextField.delegate = self;
+    
+    // Set phone number text field to open numeric keyboard
+    self.phoneNumberTextField.keyboardType = UIKeyboardTypePhonePad;
+    
+    // If editing an emergency contact
+    if (self.emergencyContact != NULL) {
+        // Populate form if contact already exists
+        [self.nameTextField setText: self.emergencyContact.name];
+        [self.phoneNumberTextField setText: self.emergencyContact.phoneNumber];
+        
+        // Set the button and title text to edit
+        [self.submitButton.titleLabel setText:@"Update"];
+        self.navigationTitle.title = @"Update Emergency Contact";
+    }
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return NO;
+}
+
+- (void)onSubmitButtonClick {
+    if (self.emergencyContact == NULL) {
+        // Add new one
+    } else {
+        // Update contact
+    }
+}
+
+@end
