aa200a2dc1a79232c5d4756afdd4b62075064d34
Adam Lewis <mr_adam_lewis@sky.com>
2016-01-17 12:53:01 +0000

    Initial Commit

---
+++ RouteBuddy/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  RouteBuddy
+//
+//  Created by Adam Lewis on 17/01/2016.
+//  Copyright © 2016 The University of Sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ RouteBuddy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RouteBuddy
+//
+//  Created by Adam Lewis on 17/01/2016.
+//  Copyright © 2016 The University of Sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
